37b2f94372a2c1c6ef74b052def3c2995cfbcaf0
D-R-Ellorinco <ellorinco.dhanrenzbscs2024@gmail.com>
2026-02-09 11:20:24 +0800

    ewan ko kay renz

---
+++ YourApp.DataAccess/DatabaseConnection.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace YourApp.DataAccess
+{
+    public class DatabaseConnection  // Moved class declaration to top
+    {
+        private static string connectionString =
+            ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;  // Changed to "MyDb"
+
+        // Method to get a new connection
+        public static MySqlConnection GetConnection()  // Changed to MySqlConnection
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        // Test connection method
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (MySqlConnection conn = GetConnection())  // Changed to MySqlConnection
+                {
+                    conn.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection failed: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
